f786ca9d38aa3947fada728a8e00a70d64133d55
Zhangguinan <1666818478@qq,com>
2019-04-17 16:19:06 +0800

    shit

--- test/Process&&Threading/协程理解.py
+++ test/Process&&Threading/协程理解.py
@@ -3,3 +3,18 @@ import threading
 def run():
     # 空变量，存储的作用data始终为空
     data = ""
+    r = yield data
+    print(1,r,data)
+    r = yield data
+    print(2,r,data)
+    r = yield data
+    print(3,r,data)
+
+m = run()
+# 启动生成器，即启动协程
+print(m.send(None))
+print(m.send('a'))
+print(m.send('b'))
+# print(m.send('c'))
+# print(m.send('d'))
+print('xxxxxx')
